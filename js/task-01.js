480c4a07a0df0afff371b9689bea809070e53651
l-Bohdan-l <bpapushoj@gmail.com>
2021-09-06 22:13:03 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -13,3 +13,15 @@ const getCategoriesChildren = function (array) {
 }
 
 getCategoriesChildren(allItem);
+
+
+/////////// вариант Ментора //////////////
+
+// const list = document.querySelector('#categories');
+// const items = list.querySelectorAll('.item');
+// console.log(`В списке ${items.length} категории:`);
+// items.forEach(node => {
+// const h2 = node.querySelector('h2');
+// const nodeItems = node.querySelectorAll('li');
+// console.log(` - категория: ${h2.textContent} (количество элементов: ${nodeItems.length})`);
+// });
